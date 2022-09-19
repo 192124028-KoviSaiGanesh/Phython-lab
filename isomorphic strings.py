fd076094e4fcf3ceab8565dab8576b13083a9d15
Ganesh-4028 <112798562+Ganesh-4028@users.noreply.github.com>
2022-09-19 17:10:38 +0530

    Create isomorphic strings.py
    1.Input: s = "egg", t = "add"
     Output: true
    2.Input: s = "foo", t = "bar"
     Output: false
    3.Input: s = "paper", t = "title"
     Output: true
    4.Input: s = "fry", t = "sky"
     Output: true
    5. Input: s = "apples", t = "apple"
     Output: false

---
+++ isomorphic strings.py
@@ -0,0 +1,17 @@
+def iso(s1,s2):
+    if len(s1) != len(s2):
+        return False
+    else:
+        map1, map2={},{}
+        for i in range(len(s1)):
+          ch1, ch2 = s1[i],s2[i]
+          if ch1 not in map1:
+              map1[ch1]=ch2
+          if ch2 not in map2:
+              map2[ch2]=ch1
+          if ((map1[ch1] !=ch2) or (map2[ch2]!=ch1)):
+              return False
+    return True
+s1=input("Enter String 1 : ")
+s2=input("Enter String 2 : ")
+print(iso(s1,s2))
